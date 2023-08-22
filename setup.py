9fa938d504006874ccd46f6a187315ea68ca60b6
lisaifei <lisaifei@cvte.com>
2023-08-22 15:40:44 +0800

    feat: support command line

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='sw-monkey',
-    version='0.1',
-    packages=['src'],
+    name='swmonkey',
+    version='0.3',
+    packages=['swmonkey'],
     author='Li Saifei',
     author_email='waltermitty121906@gmail.com',
     description='A tool for monkey test on Linux GUI',
+    entry_points={
+        'console_scripts': [
+            'swmonkey = swmonkey.__main__:main'
+        ]
+    },
     install_requires=[
         'pyautogui',
         'psutil',
-
     ]
 )
