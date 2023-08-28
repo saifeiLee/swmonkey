4a1da36b87ff20280a03d58ce209db00d04de7dd
lisaifei <lisaifei@cvte.com>
2023-08-28 16:57:49 +0800

    feat: add log message for auto-upload script

--- auto-upload.sh
+++ auto-upload.sh
@@ -1,5 +1,10 @@
+echo "准备开始构建和发布..."
+echo "清理dist目录..."
 rm -r dist/*
+echo "构建..."
 python setup.py sdist
 API_TOKEN=$(cat ~/.pypirc | grep password | cut -d' ' -f3)
 echo $API_TOKEN
+echo "上传..."
 twine upload -u __token__ -p $API_TOKEN  dist/*
+echo "完成√"
