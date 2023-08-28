cb946412ce24a89de7ce074b9afdc14a2ecc026a
lisaifei <lisaifei@cvte.com>
2023-08-28 17:30:27 +0800

    fix:打包产物的文件夹结构问题

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='swmonkey',
-    version='0.6',
-    packages=['swmonkey'],
+    version='0.9',
+    packages=find_packages(),
     author='Li Saifei',
     author_email='waltermitty121906@gmail.com',
     description='A tool for monkey test on Linux GUI',
